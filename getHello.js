273badbb399eb6a5b2a7af392db64c6cbdef5a1d
xiangqiao123 <572469094@qq.com>
2015-03-31 05:24:27 -0700

    add

---
+++ getHello.js
@@ -0,0 +1,10 @@
+var Hello=require('./hello');
+
+hello1 = new Hello();
+hello1.setName('xiangqiao');
+hello1.sayHello();
+
+hello2=new Hello();
+hello2.setName('xiangqiaoqiao');
+hello2.sayHello();
+
---
+++ loadmodule.js
@@ -0,0 +1,7 @@
+var hello1 = require('./module');
+hello1.setName('xiangqiao');
+
+var hello2=require('./module');
+hello2.setName('xiangqiaoqiao');
+
+hello1.sayHello();
